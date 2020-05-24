e63b09470d17e6affd8460200940b4eba7c1b2d2
Tom Kota <63372913+tom2kota@users.noreply.github.com>
2020-05-24 21:40:59 +0300

    Add files via upload

---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Login from "../components/Login";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Login/>
+            </div>
+        );
+    }
+}
+
+export default App;
