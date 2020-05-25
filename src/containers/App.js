3096a9a28746e57ee63fb4ae9efbbb958cfe8e40
Tom Kota <63372913+tom2kota@users.noreply.github.com>
2020-05-26 01:15:50 +0300

    Add files via upload

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import './App.css';
-import Login from "../components/Login";
+import React, {Component} from 'react';
+import Clock from "../components/Clock";
+import LoginState from "../components/LoginState";
+import LoginStateHooks from "../components/LoginStateHooks";
+import LoginPropsOne from "../components/LoginPropsOne";
+import LoginPropsTwo from "../components/LoginPropsTwo";
+import LoginLayout from "../components/LoginLayout";
 
-class App extends React.Component {
+import './App.css';
+
+
+class App extends Component {
+
+    callbackFunction = (childData) => {
+        alert(`From parent App component, childData = ${childData}`)
+        this.setState({
+            dataStore: childData
+        })
+    }
+
     render() {
+        console.log('%c 0) App (State)', 'background: gray; color: white; display: block;');
+        console.log('App (state):', this.state);
+        console.log('App (props):', this.props);
+
         return (
             <div className="App">
-                <Login/>
+                <Clock parentCallback={this.callbackFunction}/>
+                <br/>
+                <br/>
+                <LoginState/>
+                <br/>
+                <br/>
+                <LoginStateHooks/>
+                <br/>
+                <br/>
+                <LoginPropsOne/>
+                <br/>
+                <br/>
+                <LoginPropsTwo/>
+                <br/>
+                <br/>
+                <LoginLayout/>
             </div>
         );
     }
